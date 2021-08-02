43b7ae6f8a331beb721ae750b8032920e492763a
todor <todor.ivanov.work@gmail.com>
2021-08-02 23:07:00 +0300

    Initial commit. Added a bunch of functions that are yet to be populated. Apart from this the DB.connect() and DB.init_db() are probably fully done, including error catching and reporting.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import pymongo.errors
 from pydantic import BaseModel
 import datetime
 from pymongo import MongoClient
@@ -59,42 +62,73 @@ def send_to_logger(err_type, message, client_id="None", client_email="None"):
 class DB:
 
     @staticmethod
-    def init():
+    def init_db():
+        # Here we start the MongoDB connection using the MongoClient via the "connect()" function
+        # This returns the __mongo_db__ Mongo database object
         db = DB.connect()
-        states_collection = db['states']
-
+        # A collection in MongoDB is the same as a table in MySQL and her we declare a "user_states" collection
+        states_collection = db['user_states']
+        # Here we prepare a timestamp for when the collection was created.
+        # Unless some document (row) is saved within a collection, Mongo won't save it to the disk and remains within the memory
         temp_time = datetime.datetime.utcnow()
         now = temp_time.strftime("%b-%d-%Y-%H:%M")
-
         init_document = {"init_time": str(now)}
-
+        # Here we insert the init_document we setup above
         result = states_collection.insert_one(init_document)
 
-        db_list = db.list_database_names()
-        if __mongo_host__ in db_list:
-            db_collections = db.list_collection_names()
-            if "states" in db_collections:
+        # Once this is done we need to verify that the setup has been completed
+        db_names_list = db.list_database_names()
+        if __mongo_db__ in db_names_list:
+            db_collection_names = db.list_collection_names()
+            if "states" in db_collection_names:
                 return {
                     "Response": "Success",
                     "Message": "MongoDB Initialized"
                 }
+            # If the "states" collection doesn't exist:
             else:
+                message = "[Cluster][Mongo][Error][04]: Can't select the 'states' collection!"
+                send_to_logger("error", message, client_id="None", client_email="None")
                 return {
                     "Response": "Error",
-                    "Message": "MongoDB Init Failed"
+                    "Message": message
                 }
+        # If the database doesn't exist or can't be selected
         else:
+            message = "[Cluster][Mongo][Error][03]: Can't select DB!"
+            send_to_logger("error", message, client_id="None", client_email="None")
             return {
                 "Response": "Error",
-                "Message": "MongoDB Init Failed"
+                "Message": message
             }
 
 
     @staticmethod
     def connect():
-        client = MongoClient(__mongo_host__)
-        db = client[__mongo_db__]
-        return db
+        try:
+            client = MongoClient(__mongo_host__)
+            db = client[__mongo_db__]
+            if db is None:
+                return {
+                    "Response": "Error",
+                    "Message": "[Cluster][Mongo][Error][02]: Can't open the DB!"
+                }
+            else:
+                return db
+        except pymongo.errors.PyMongoError as err:
+            message = "[Cluster][Mongo][Error][01]: Can't connect to the Mongo service. Full Error: " + str(err)
+            send_to_logger("error", message, client_id="None", client_email="None")
+            return {
+                "Response": "Error",
+                "Message": message
+            }
+
+
+
+    @staticmethod
+    def execute_command(database_obj, command: str, data: Optional[list] = []):
+        db_client = DB.connect()
+
 
 
 class PostStateGet(BaseModel):
@@ -111,11 +145,22 @@ class PostStateSet(BaseModel):
     state_obj: dict
 
 
+class Cluster:
+
+    @staticmethod
+    def user_state_get(client_id: str):
+        db_conn = DB.connect()
+        db_coll = db_conn['user_states']
+        db_command = {"client_id": client_id}
+        db_result = db_coll.find(db_command)
+        for item in db_result:
+            pprint(item)
+
+
 @app.post("/state/get", status_code=200)
 def cluster_state_get(post_data: PostStateGet):
     post_data_dict = post_data.dict()
-
-
+    # To be continued
 
 
 @staticmethod
